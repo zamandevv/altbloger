f938b88ad0c611394d2947f3ed02d886443fdebd
Adebayo Nurudeen <adebayonur@gmail.com>
2022-11-08 18:05:55 +0100

    updated files

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const DBURI = process.env.NODE_ENV === 'test' ? process.env.TEST_DBURI : process.env.DBURI
+const DBURI = process.env.DBURI
 const PORT = process.env.PORT
 const SECRET = process.env.SECRET
 
